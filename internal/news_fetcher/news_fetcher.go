2ffc2c80dc71c321d2ab4f326800006a0c741762
Yuriy Dubinin <yuradubinin100@gmail.com>
2025-02-28 20:29:21 +0300

    add

--- internal/news_fetcher/news_fetcher.go
+++ internal/news_fetcher/news_fetcher.go
@@ -25,9 +25,14 @@ func NewNewsFetcher() *NewsFetcher {
 }
 
 func (nf *NewsFetcher) FetchLastNews() ([]dataFormatter.NewsMap, error) {
-	err := godotenv.Load()
+	envPath := os.Getenv("ENV_FILE")
+	if envPath == "" {
+		envPath = ".env"
+	}
+
+	err := godotenv.Load(envPath)
 	if err != nil {
-		return nil, fmt.Errorf("[NEWS_FETCHER]: Ошибка загрузки .env файла: %v", err)
+		log.Fatal("[NEWS_FETCHER]: Ошибка загрузки .env")
 	}
 
 	apiKey := os.Getenv("NEWS_API_KEY")
